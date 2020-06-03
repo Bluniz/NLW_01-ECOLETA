b6fe723da22249017369dff6d2d169630886401a
Lucas Rosa <55213609+Bluniz@users.noreply.github.com>
2020-06-03 02:15:49 -0300

    Correção do bug de inserir dados no banco com ids inexistentes

--- Server/src/database/connection.ts
+++ Server/src/database/connection.ts
@@ -9,6 +9,11 @@ const connection = knex({
   connection: {
     filename: path.resolve(__dirname, 'database.sqlite') 
   },
+  pool: {
+    afterCreate:(connection: any, done: Function) => {
+      connection.run('PRAGMA  foreign_keys = ON', done);
+    }
+  },
 
   useNullAsDefault: true
 });

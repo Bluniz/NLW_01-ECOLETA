bb8bed27fa095b3977c31a81bdcf16286f038b7b
Lucas Rosa <55213609+Bluniz@users.noreply.github.com>
2020-06-06 18:36:00 -0300

    +Depêndencias multer, multerconfig

--- Server/src/routes.ts
+++ Server/src/routes.ts
@@ -1,17 +1,26 @@
 import express from 'express';
-
+import multer from 'multer';
+import multerConfig from './config/multer';
 import PointsController from './controllers/PointsController';
 import ItemsController from './controllers/ItemsController';
 
 
+
 const routes = express.Router();
+const upload = multer(multerConfig);
+
+
 const pointsController = new PointsController();  
 const itemsController = new ItemsController();
 
+//Rota para pegar as imagens de items
 routes.get('/items', itemsController.index);
-
-routes.post('/points', pointsController.create);
+/*Rota para cadastrar pontos - upload.single('image') é o campo para
+passar a imagem como parâmetro na rota*/
+routes.post('/points',upload.single('image'), pointsController.create);
+//Rota para pegar todos os pontos filtrados
 routes.get('/points/', pointsController.index);
+//Rota para pegar um ponto especifico
 routes.get('/points/:id', pointsController.show);
   
  

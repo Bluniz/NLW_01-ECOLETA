916703f5f38d9ef8da89e10382cc7ea733c876fd
Lucas Rosa <55213609+Bluniz@users.noreply.github.com>
2020-06-06 20:02:10 -0300

    Adicionado Celebrate para validação de dados no backend!

--- Server/src/routes.ts
+++ Server/src/routes.ts
@@ -3,6 +3,7 @@ import multer from 'multer';
 import multerConfig from './config/multer';
 import PointsController from './controllers/PointsController';
 import ItemsController from './controllers/ItemsController';
+import {celebrate, Joi} from "celebrate";
 
 
 
@@ -17,7 +18,26 @@ const itemsController = new ItemsController();
 routes.get('/items', itemsController.index);
 /*Rota para cadastrar pontos - upload.single('image') é o campo para
 passar a imagem como parâmetro na rota*/
-routes.post('/points',upload.single('image'), pointsController.create);
+routes.post(
+  '/points',
+  upload.single('image'),
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      whatsapp: Joi.number().required(),
+      latitude: Joi.number().required(),
+      longitude: Joi.number().required(),
+      city: Joi.string().required(),
+      uf: Joi.string().required().max(2),
+      items: Joi.string().required(),
+    })
+  },{
+    abortEarly: false
+  }),
+
+   pointsController.create
+   );
 //Rota para pegar todos os pontos filtrados
 routes.get('/points/', pointsController.index);
 //Rota para pegar um ponto especifico
